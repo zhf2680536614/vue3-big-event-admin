992e71f3741489c81b17b0e7a9bae49005293109
zhf2680536614 <zhf19993612440@outlook.com>
2025-01-13 10:20:49 +0800

    initPlus

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+import '@/assets/main.scss'
+
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia()
+
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
