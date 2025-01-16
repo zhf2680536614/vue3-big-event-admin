f3c2b60cd09f9bd9285814558fb58cdb12cc5c77
zhf2680536614 <zhf19993612440@outlook.com>
2025-01-16 11:41:51 +0800

    获取用户信息 文章分类与文章管理的架子搭建-插槽

---
+++ src/api/user.js
@@ -0,0 +1,12 @@
+import request from '@/utils/request'
+
+// 用户注册
+export const userRegisterService = ({ username, password, repassword }) =>
+  request.post('/api/reg', { username, password, repassword })
+
+// 用户登录
+export const userLoginService = ({ username, password }) =>
+  request.post('/api/login', { username, password })
+
+// 获取用户信息
+export const userGetInfoService = () => request.get('/my/userinfo')
--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -1,4 +1,222 @@
+<script setup>
+import { useRouter } from 'vue-router'
+import { User, Lock } from '@element-plus/icons-vue'
+import { ref, watch } from 'vue'
+const isRegister = ref(false)
+
+watch(isRegister, () => {
+  userForm.value = {
+    username: '',
+    password: '',
+    repassword: ''
+  }
+})
+
+//表单校验
+// 1.非空校验
+// 2.长度校验
+// 3.正则校验
+// 4.自定义校验
+
+const form = ref(null)
+
+const userForm = ref({
+  username: '',
+  password: '',
+  repassword: ''
+})
+
+const rules = {
+  username: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+    { min: 5, max: 10, message: '用户名长度必须是 5-10 位之间的字符' }
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    {
+      pattern: /^\S{6,15}$/,
+      message: '必须是 6-15 位的非空字符',
+      trigger: 'blur'
+    }
+  ],
+  repassword: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    {
+      pattern: /^\S{6,15}$/,
+      message: '必须是 6-15 位的非空字符',
+      trigger: 'blur'
+    },
+    {
+      validator: (rule, value, callback) => {
+        if (value !== userForm.value.password) {
+          return callback(new Error('再次输入密码不一致'))
+        }
+        callback()
+      },
+      trigger: 'blur'
+    }
+  ]
+}
+
+//注册
+import { userRegisterService, userLoginService } from '@/api/user'
+const register = async () => {
+  await form.value.validate()
+  await userRegisterService(userForm.value)
+  ElMessage.success('注册成功')
+  isRegister.value = !isRegister.value
+}
+
+const router = useRouter()
+import { useUserStore } from '@/store'
+const login = async () => {
+  await form.value.validate()
+  const res = await userLoginService(userForm.value)
+  console.log(res)
+  ElMessage.success('登录成功')
+  const userStore = useUserStore()
+  userStore.setToken(res.data.token)
+  router.push('/')
+}
+</script>
+
 <template>
-  <div>登陆页面</div>
+  <el-row class="login-page">
+    <el-col :span="12" class="bg"></el-col>
+    <el-col :span="6" :offset="3" class="form">
+      <el-form
+        ref="form"
+        :model="userForm"
+        :rules="rules"
+        size="large"
+        autocomplete="off"
+        v-if="isRegister"
+      >
+        <el-form-item>
+          <h1>注册</h1>
+        </el-form-item>
+        <el-form-item prop="username">
+          <el-input
+            v-model="userForm.username"
+            :prefix-icon="User"
+            placeholder="请输入用户名"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            :prefix-icon="Lock"
+            v-model="userForm.password"
+            type="password"
+            placeholder="请输入密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="repassword">
+          <el-input
+            :prefix-icon="Lock"
+            type="password"
+            v-model="userForm.repassword"
+            placeholder="请输入再次密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            @click="register"
+            class="button"
+            type="primary"
+            auto-insert-space
+          >
+            注册
+          </el-button>
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link type="info" :underline="false" @click="isRegister = false">
+            ← 返回
+          </el-link>
+        </el-form-item>
+      </el-form>
+      <el-form
+        ref="form"
+        :model="userForm"
+        :rules="rules"
+        size="large"
+        autocomplete="off"
+        v-else
+      >
+        <el-form-item>
+          <h1>登录</h1>
+        </el-form-item>
+        <el-form-item prop="username">
+          <el-input
+            v-model="userForm.username"
+            :prefix-icon="User"
+            placeholder="请输入用户名"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            v-model="userForm.password"
+            name="password"
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="请输入密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item class="flex">
+          <div class="flex">
+            <el-checkbox>记住我</el-checkbox>
+            <el-link type="primary" :underline="false">忘记密码？</el-link>
+          </div>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            @click="login"
+            class="button"
+            type="primary"
+            auto-insert-space
+            >登录</el-button
+          >
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link type="info" :underline="false" @click="isRegister = true">
+            注册 →
+          </el-link>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
 </template>
-<script setup></script>
+
+<style lang="scss" scoped>
+.login-page {
+  height: 100vh;
+  background-color: #fff;
+
+  .bg {
+    background:
+      url('@/assets/logo2.png') no-repeat 60% center / 240px auto,
+      url('@/assets/login_bg.jpg') no-repeat center / cover;
+    border-radius: 0 20px 20px 0;
+  }
+
+  .form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    user-select: none;
+
+    .title {
+      margin: 0 auto;
+    }
+
+    .button {
+      width: 100%;
+    }
+
+    .flex {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+}
+</style>
